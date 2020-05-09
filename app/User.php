672758d66d82cd646cfa6d39d14395c9740936ab
Franklyn Kleiton <franklynkleiton18@gmail.com>
2020-05-09 16:20:42 -0300

    :recycle: Improving Create User Code

--- app/User.php
+++ app/User.php
@@ -2,14 +2,38 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable
 {
-    protected $table = 'usuarios';
     protected $guarded = [];
-    public $timestamps = false;
+    protected $table = 'usuarios';
     protected $hidden = ['senha'];
+    public $timestamps = false;
+
+    /**
+     * The booted method of the model
+     *
+     * @return void
+     */
+    public static function booted()
+    {
+        static::creating(function ($user) {
+            $user->senha = Hash::make($user->senha);
+        });
+    }
+
+    /**
+     * Checks whether the user is an admin.
+     *
+     * If the user has the role 1, he will be
+     * treated as a admin by the application.
+     *
+     * @return bool
+     */
+    public function isAdmin(): bool
+    {
+        return intval($this->role) === 1;
+    }
 }
