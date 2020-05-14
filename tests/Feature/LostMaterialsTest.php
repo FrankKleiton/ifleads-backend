5f0e6a0255104634df8ff6a0ed24208fba7a29be
HermersonDev <ne.hermerson@gmail.com>
2020-05-14 19:42:29 -0300

    :construction: create test to validate LostMaterial fields

--- tests/Feature/LostMaterialsTest.php
+++ tests/Feature/LostMaterialsTest.php
@@ -12,7 +12,6 @@ class LostMaterialsTest extends TestCase
     /** @test */
     public function shouldCreateANewLostMaterial()
     {
-
         $body = [
             'nome' => 'Lost Material Test',
             'descricao' => 'Lost material only for test',
@@ -27,5 +26,33 @@ class LostMaterialsTest extends TestCase
                 'descricao' => $body['descricao'],
                 'matriculaDeQuemEntregou' => $body['matriculaDeQuemEntregou']
             ]);
+    }
+
+    /** @test */
+    public function shouldCheckTypesOfInputsWhenForCreateANewLostMaterial()
+    {
+        $bodyWithNameTypeIncorrect = [
+            'nome' => 123,
+            'descricao' => 'Lost material only for test',
+            'matriculaDeQuemEntregou' => '20161038060041'
+        ];
+        $response = $this->postJson('/api/materials/losts', $bodyWithNameTypeIncorrect);
+        $response->assertStatus(422);
+
+        $bodyWithDescriptionTypeIncorrect = [
+            'nome' => 'Lost Material Test',
+            'descricao' => 123,
+            'matriculaDeQuemEntregou' => '20161038060041'
+        ];
+        $response = $this->postJson('/api/materials/losts', $bodyWithDescriptionTypeIncorrect);
+        $response->assertStatus(422);
+
+        $bodyWithMatriculaTypeIncorrect = [
+            'nome' => 'Lost Material Test',
+            'descricao' => 'Lost material only for test',
+            'matriculaDeQuemEntregou' => 20161038060041
+        ];
+        $response = $this->postJson('/api/materials/losts', $bodyWithMatriculaTypeIncorrect);
+        $response->assertStatus(422);
     }
 }
